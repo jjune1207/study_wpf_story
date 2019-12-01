274206495713514a4e0be7e16d4b70b794df08ed
YoungChulShin <go1323@gmail.com>
2019-12-02 06:26:54 +0900

    01.StringFormat

---
+++ 01.StringFormat/Main.xaml.cs
@@ -0,0 +1,18 @@
+﻿using _01.StringFormat.ViewModel;
+using System.Windows;
+
+namespace _01.StringFormat
+{
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    public partial class Main : Window
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+            this.DataContext = new MainViewModel();
+        }
+    }
+}
---
+++ 01.StringFormat/ViewModel/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01.StringFormat.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private DateTime _currentTime;
+        public DateTime CurrentTime
+        {
+            get { return _currentTime; }
+            set { SetProperty(ref _currentTime, value); }
+        }
+
+        private int _currentBudget;
+        public int CurrentBudget
+        {
+            get { return _currentBudget; }
+            set { SetProperty(ref _currentBudget, value); }
+        }
+
+        public MainViewModel()
+        {
+            CurrentTime = DateTime.Now;
+            CurrentBudget = 1000;
+        }
+
+    }
+}
